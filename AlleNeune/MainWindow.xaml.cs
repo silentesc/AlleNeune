384fc8df7d819c1996896dc30cfdb6e0aa1880bb
silentesc <scope-frisk-copy@duck.com>
2023-09-21 17:27:55 +0200

    Finish design and add button click listeners

--- AlleNeune/MainWindow.xaml.cs
+++ AlleNeune/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace AlleNeune
 {
@@ -7,6 +9,20 @@ namespace AlleNeune
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // When any game button is clicked
+        private void ButtonClickEvent(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine(((Button)sender).Name);
+        }
+
+        // When the "Start Game" button is clicked
+        private void ButtonClickStartEvent(object sender, RoutedEventArgs e)
+        {
+            Button startButton = (Button)sender;
+            startButton.IsEnabled = false;
+            startButton.Content = "Game running...";
         }
     }
 }
